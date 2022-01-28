7c66af6caf5673b678a310e3a973c01f1d498f32
Prithiviraj <59286864+Prithivi-Raj@users.noreply.github.com>
2022-01-28 16:54:53 +0530

    case ZXZ

--- rotmat2eulang.m
+++ rotmat2eulang.m
@@ -39,6 +39,12 @@ switch order
     eul(2) = -asin(R(3,1));
     eul(3) = atan2(R(3,2),R(3,3));
     
+  case 'ZXZ'
+    eul(1) = acos(R(3,3));
+    eul(2) = atan2(R(3,1),R(3,2));
+    eul(3) = atan2(-R(1,3),R(2,3));
+    
+    
   otherwise
     error('Don''t know specified rotation order %s', order)
 
